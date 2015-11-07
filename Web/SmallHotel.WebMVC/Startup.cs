30a46cc948bae212fdf2ea46a09febc2519f1bc4
Ivan Tochev <annihilatora@gmail.com>
2015-11-08 00:52:13 +0200

    Created empty data and web projects

---
+++ Web/SmallHotel.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SmallHotel.WebMVC.Startup))]
+namespace SmallHotel.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
